9c3acc03734b244a28f7213d5be02f9d3b222692
bgzerol <1600494183@qq.com>
2026-09-19 16:40:26 +0900

    user_lib 接入 main

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
+extern void app_init(void);
+
 void app_main(void)
 {
-
+    app_init();
 }
---
+++ esp32/user_lib/app.cpp
@@ -0,0 +1,7 @@
+/**
+ * @brief 应用程序初始化函数
+ */
+extern "C" void app_init(void)
+{
+    ;
+}
